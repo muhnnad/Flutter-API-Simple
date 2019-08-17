ccbad94ad5fd9b6cc3727f5043bf1dfd9908a0fc
Emma Paris <eparis@atlassian.com>
2019-08-17 04:13:52 +0300

    First commit

--- "API" is simple/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'HomePage.dart';
-
-// import 'HomePage.dart';
-void main() => runApp(MaterialApp(
-      home: MyApp(),
-      theme: ThemeData.dark(),
-    ));
